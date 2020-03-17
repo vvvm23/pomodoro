17dac2f55c18f0d828cdee89aaaa64551aa51ab7
Alexander McKinney <alexander.f.mckinney@durham.ac.uk>
2020-03-17 00:59:07 +0000

    added README

---
+++ README.md
@@ -0,0 +1,20 @@
+# Pomodoro Script
+
+Simply Python script to keep tracking of timings when studying
+
+## Usage
+
+`python pomodoro.py [-h] [--wt WT] [--rt RT] [-n N] [--wc WC] [--rc RC]`
+
+When a stage is complete, press enter to continue when ready.
+
+## Arguments
+
+```
+  -h, --help  show this help message and exit
+  --wt WT     Amount of time on focused work in minutes.
+  --rt RT     Amount of time resting in minutes.
+  -n N        Number of cycles to do.
+  --wc WC     Color of the progress bar in work mode.
+  --rc RC     Color of the progress bar in rest mode.
+```
--- pomodoro.py
+++ pomodoro.py
@@ -30,7 +30,7 @@ def main(work_time, rest_time, n, work_color, rest_color):
                     time.sleep(1)
                     pbar.update(1)
 
-            input(colors['reset'] + "Press any key to continue to resting..")
+            input(colors['reset'] + "Press ENTER to continue to resting..")
             if n != None and i==n-1:
                 break
 
@@ -40,7 +40,7 @@ def main(work_time, rest_time, n, work_color, rest_color):
                 for t in range(rest_time):
                     time.sleep(1)
                     pbar.update(1)
-            input(colors['reset'] + "Press any key to continue working..")
+            input(colors['reset'] + "Press ENTER to continue working..")
     except KeyboardInterrupt as e:
         print(colors['reset'])
         print("Thanks for studying with me!")
@@ -58,7 +58,6 @@ if __name__ == '__main__':
     parser.add_argument("-n", help="Number of cycles to do.", default=None)
     parser.add_argument("--wc", help="Color of the progress bar in work mode.", default='red')
     parser.add_argument("--rc", help="Color of the progress bar in rest mode.", default='green')
-
 
     args = parser.parse_args()
     main(int(args.wt)*60, int(args.rt)*60, None if args.n == None else int(args.n), args.wc, args.rc)
